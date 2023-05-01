7ddd80f7a09c5615ef05310dfa1c32609f79ad88
davidsongeller <111794108+davidsongeller@users.noreply.github.com>
2023-05-01 12:34:53 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,29 @@
 import textwrap
 
-# Get input from user
-long_text = input("Enter a long line of text: ")
+def format_input(input_string, max_width):
+    output_lines = textwrap.wrap(input_string, width=max_width, replace_whitespace=False)
+    final_output = "\n".join(output_lines)
+    return final_output
 
-# Define maximum width for shorter lines
-width = 40
+def get_input():
+    while True:
+        input_string = input("Enter a long line of text: ")
+        if input_string:
+            return input_string
 
-# Wrap long text into shorter lines
-wrapped_text = textwrap.fill(long_text, width=width, replace_whitespace=False)
+def get_max_width():
+    while True:
+        max_width_input = input("Enter the maximum width for each line: ")
+        if max_width_input.isdigit():
+            return int(max_width_input)
+        else:
+            print("Invalid input. Please enter a valid integer.")
 
-# Print the wrapped text
-print(wrapped_text)
+def main():
+    input_string = get_input()
+    max_width = get_max_width()
+    formatted_input = format_input(input_string, max_width)
+    print(formatted_input)
+
+if __name__ == '__main__':
+    main()
